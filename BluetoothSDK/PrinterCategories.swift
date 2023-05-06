2597f8d5b06b36779541afdd4ec8a03432a336a1
Ariful0007 <arifulpub143@gmail.com>
2023-05-06 09:30:12 +0600

    commit--

---
+++ BluetoothSDK/PrinterCategories.swift
@@ -0,0 +1,21 @@
+//
+//  PrinterCategories.swift
+//  BluetoothSDK
+//
+//  Created by sang on 15/10/1444 AH.
+//
+
+import UIKit
+
+class PrinterCategories: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
--- BluetoothSDK/ViewController.swift
+++ BluetoothSDK/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gonext: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func mygonext(_ sender: UIButton) {
+        let sec = storyboard?.instantiateViewController(identifier: "printers") as! PrinterCategories
+                    present(sec,animated: true)
+    }
+    
 }
 
