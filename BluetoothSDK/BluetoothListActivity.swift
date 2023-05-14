50b34b395f5bf4678798d7cd6b3307560c4fc475
Ariful0007 <arifulpub143@gmail.com>
2023-05-14 13:54:11 +0600

    vvv

---
+++ BluetoothSDK/BluetoothListActivity.swift
@@ -0,0 +1,29 @@
+//
+//  BluetoothListActivity.swift
+//  BluetoothSDK
+//
+//  Created by sang on 23/10/1444 AH.
+//
+
+import UIKit
+
+class BluetoothListActivity: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
